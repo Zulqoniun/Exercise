aa6ae2fbf6fe68190323ebedec31f8fc073bb46c
Zulqoniun <muhammadqoni123@gmail.com>
2020-04-21 23:02:46 +0700

    exercise

---
+++ app/src/main/java/com/example/exercise/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.exercise;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.content.Intent;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "MESSAGE";
+    private ListView obj;
+    private FloatingActionButton fab;
+    com.example.exercisesqllite.DBHelper mydb;
+,',
+
+        '
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mydb = new com.example.exercisesqllite.DBHelper(this);
+        ArrayList array_list = mydb.getAllCotacts();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, array_list);
+
+        obj = (ListView) findViewById(R.id.lv1);
+        obj.setAdapter(arrayAdapter);
+        obj.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                int id_To_Search = arg2 + 1;
+
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+
+                Intent intent = new Intent(getApplicationContext(), profil.class);
+
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
+
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, com.example.exercisesqllite.displaymhs.class));
+            }
+        });
+
+
+    }
+
+
+
+    private void showDialogg() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set title dialog
+
+
+        // set pesan dari dialog
+        final String[] dialog = {"Hitung Luas", "Hitung Keliling",};
+        alertDialogBuilder.setItems(dialog, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0: {
+
+
+                    }
+                    break;
+                    case 1: {
+
+                    }
+                    break;
+                    default:
+
+                }
+            }
+        });
+
+        // membuat alert dialog dari builder
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // menampilkan alert dialog
+        alertDialog.show();
+    }
+
+
+}
